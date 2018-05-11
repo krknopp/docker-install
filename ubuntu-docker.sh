80ee25bec4fb779ec0aac022da57dd59db008648
Kerry Knopp <kknopp@Kerrys-iMac.local>
2018-05-11 09:04:15 -0500

    updating for 16.04

--- ubuntu-docker.sh
+++ ubuntu-docker.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+#install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-apt-get update
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
-apt-get install -y linux-image-extra-$(uname -r) docker-engine ntp nfs-common
+apt update
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+apt install -y docker-ce chrony
+
+
+#install Docker-Compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
+
